6ae9eb6ab1ab9f750a384bbadeb9457b8ac17566
CMike98 <miccze18@gmail.com>
2024-08-13 18:59:39 +0200

    Game State beginnings

--- GameState.cs
+++ GameState.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace CluelessControl
 {
-    internal class GameState
+    public class GameState
     {
+        #region Instance
+        private static object _lock = new();
+        private static GameState? _instance = null;
+
+        public static GameState Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    _instance ??= new GameState();
+                    return _instance;
+                }
+            }
+        }
+        #endregion
+
+        public GameSettings Settings
+        {
+            get;
+            private set;
+        }
+
+        private GameState()
+        {
+            Settings = new GameSettings();
+        }
     }
 }
