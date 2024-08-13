a0cba2e0aebce69a709d7783b66b5bb433b3e62b
CMike98 <miccze18@gmail.com>
2024-08-13 12:01:13 +0200

    Question classes

---
+++ JsonQuestionConverter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CluelessControl
+{
+    public class JsonQuestionConverter : JsonConverter<Question>
+    {
+        public override Question? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            using var jsonDoc = JsonDocument.ParseValue(ref reader);
+            var root = jsonDoc.RootElement;
+
+            string text = root.GetProperty("text").GetString() ?? throw new JsonException("No question text.");
+
+            string[] optionsArray = root.GetProperty("answers")
+                .EnumerateArray()
+                .Select(answer =>
+                {
+                    string? answerText = answer.GetString();
+                    if (string.IsNullOrWhiteSpace(answerText))
+                        throw new JsonException("At least answer is null or white space.");
+                    return answerText;
+                })
+                .ToArray();
+
+            if (optionsArray.Length != Constants.ANSWERS_PER_QUESTION)
+                throw new JsonException($"There must be exactly {Constants.ANSWERS_PER_QUESTION} answers.");
+
+            int correctAnswerIndex = root.GetProperty("correctAnswerIndex").GetInt32();
+            if (correctAnswerIndex < 0 || correctAnswerIndex >= Constants.ANSWERS_PER_QUESTION)
+                throw new JsonException($"The correct answer index must be between 0 and {Constants.ANSWERS_PER_QUESTION - 1}.");
+
+            return Question.Create(text, optionsArray, correctAnswerIndex);
+        }
+
+        public override void Write(Utf8JsonWriter writer, Question value, JsonSerializerOptions options)
+        {
+            // Start writing the question
+            writer.WriteStartObject();
+            
+            // Write the question text
+            writer.WriteString("text", value.Text);
+
+            // Write the possible answers
+            writer.WritePropertyName("answers");
+            writer.WriteStartArray();
+            foreach (string answer in value.Answers)
+            {
+                writer.WriteStringValue(answer);
+            }
+            writer.WriteEndArray();
+
+            // Write the correct answer
+            writer.WriteNumber("correctAnswerIndex", value.CorrectAnswerIndex);
+
+            // End writing the question
+            writer.WriteEndObject();
+        }
+    }
+}
---
+++ Question.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CluelessControl
+{
+    public class Question
+    {
+        public string Text
+        {
+            get;
+        }
+
+        public string[] Answers
+        {
+            get;
+        }
+
+        public int CorrectAnswerIndex
+        {
+            get;
+        }
+
+        private Question(string text, string[] answers, int correctAnswerIndex)
+        {
+            Text = text.Trim();
+            Answers = answers;
+            CorrectAnswerIndex = correctAnswerIndex;
+        }
+
+        public static Question Create(string text, string[] answers, int correctAnswerIndex)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                throw new ArgumentNullException(nameof(text));
+            if (answers == null)
+                throw new ArgumentNullException(nameof(answers));
+            if (answers.Length != Constants.ANSWERS_PER_QUESTION)
+                throw new ArgumentException($"There must be exactly {Constants.ANSWERS_PER_QUESTION} answers.", nameof(answers));
+            if (answers.Any(answer => string.IsNullOrWhiteSpace(answer)))
+                throw new ArgumentException("At least one answer is null or white space string.", nameof(answers));
+            if (correctAnswerIndex < 0 || correctAnswerIndex >= answers.Length)
+                throw new ArgumentOutOfRangeException(nameof(correctAnswerIndex), $"Correct answer index must be between 0 and {Constants.ANSWERS_PER_QUESTION - 1}.");
+
+            return new Question(text, answers, correctAnswerIndex);
+        }
+    }
+}
