ba1685842f070967d3f05b59906a4ce8f4be84c3
CMike98 <miccze18@gmail.com>
2024-08-13 12:03:05 +0200

    Rename the variable in JsonQuestionConverter and remove unused usings

--- JsonQuestionConverter.cs
+++ JsonQuestionConverter.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
+﻿using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
 namespace CluelessControl
 {
@@ -19,9 +14,9 @@ namespace CluelessControl
 
             string[] optionsArray = root.GetProperty("answers")
                 .EnumerateArray()
-                .Select(answer =>
+                .Select(answerElement =>
                 {
-                    string? answerText = answer.GetString();
+                    string? answerText = answerElement.GetString();
                     if (string.IsNullOrWhiteSpace(answerText))
                         throw new JsonException("At least answer is null or white space.");
                     return answerText;
