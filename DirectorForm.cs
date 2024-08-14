8f7d637b095b86669e353ca739a5a4c216914eef
CMike98 <miccze18@gmail.com>
2024-08-14 16:42:55 +0200

    Show other forms when starting up

--- DirectorForm.cs
+++ DirectorForm.cs
@@ -2,6 +2,11 @@ namespace CluelessControl
 {
     public partial class DirectorForm : Form
     {
+        #region Other Forms
+        private static readonly HostScreenForm _hostScreenForm = new();
+        private static readonly ContestantScreenForm _contestantScreenForm = new();
+        #endregion
+
         public DirectorForm()
         {
             InitializeComponent();
@@ -9,7 +14,13 @@ namespace CluelessControl
 
         private void DirectorForm_Load(object sender, EventArgs e)
         {
+            ShowAllForms();
+        }
 
+        private static void ShowAllForms()
+        {
+            _hostScreenForm.Show();
+            _contestantScreenForm.Show();
         }
     }
 }
