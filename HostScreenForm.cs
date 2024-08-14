92a838de23a3bd5f3f15476ee08e6efcc56cf254
CMike98 <miccze18@gmail.com>
2024-08-14 16:10:43 +0200

    Host Screen added

---
+++ HostScreenForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CluelessControl
+{
+    public partial class HostScreenForm : Form
+    {
+        public HostScreenForm()
+        {
+            InitializeComponent();
+        }
+
+        private void HostScreenForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
