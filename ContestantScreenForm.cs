94043aa3b765f5071a0a781c9c2e22d1ccabfe29
CMike98 <miccze18@gmail.com>
2024-08-14 16:10:53 +0200

    Contestant Screen Form

---
+++ ContestantScreenForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CluelessControl
+{
+    public partial class ContestantScreenForm : Form
+    {
+        public ContestantScreenForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ContestantScreenForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
