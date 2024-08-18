0c946fcce34032c0954826c16299f2f72fbd261e
CMike98 <miccze18@gmail.com>
2024-08-18 20:26:28 +0200

    New form - TV screen.

---
+++ TVScreenForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CluelessControl
+{
+    public partial class TVScreenForm : Form
+    {
+        public TVScreenForm()
+        {
+            InitializeComponent();
+        }
+
+        #region Form Closing
+        private void TVScreenForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            switch (e.CloseReason)
+            {
+                case CloseReason.WindowsShutDown:
+                case CloseReason.TaskManagerClosing:
+                case CloseReason.ApplicationExitCall:
+                    break;
+                default:
+                    MessageBox.Show(Constants.CLOSE_ON_DIRECTOR_FORM_MESSAGE, Constants.PROGRAM_TITLE, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    e.Cancel = true;
+                    break;
+            }
+        }
+        #endregion
+    }
+}
