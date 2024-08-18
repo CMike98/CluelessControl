06fb7a605060be30e2ffa1ef483def2008eadd18
CMike98 <miccze18@gmail.com>
2024-08-18 20:09:35 +0200

    Copy the interface from host to contestant screen

--- ContestantScreenForm.Designer.cs
+++ ContestantScreenForm.Designer.cs
@@ -28,7 +28,298 @@
         /// </summary>
         private void InitializeComponent()
         {
+            HostEnvelope4Picture = new PictureBox();
+            HostEnvelope3Picture = new PictureBox();
+            HostEnvelope2Picture = new PictureBox();
+            HostEnvelope1Picture = new PictureBox();
+            HostEnvelope0Picture = new PictureBox();
+            ContestantEnvelope0Picture = new PictureBox();
+            ContestantEnvelope3Picture = new PictureBox();
+            ContestantEnvelope2Picture = new PictureBox();
+            ContestantEnvelope1Picture = new PictureBox();
+            ContestantEnvelope4Picture = new PictureBox();
+            OfferLabel = new Label();
+            CashLabel = new Label();
+            OfferTextLabel = new Label();
+            CashTextLabel = new Label();
+            HostTextLabel = new Label();
+            ContestantTextLabel = new Label();
+            AnswerDLabel = new Label();
+            AnswerCLabel = new Label();
+            AnswerBLabel = new Label();
+            AnswerALabel = new Label();
+            QuestionLabel = new Label();
+            ((System.ComponentModel.ISupportInitialize)HostEnvelope4Picture).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)HostEnvelope3Picture).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)HostEnvelope2Picture).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)HostEnvelope1Picture).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)HostEnvelope0Picture).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)ContestantEnvelope0Picture).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)ContestantEnvelope3Picture).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)ContestantEnvelope2Picture).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)ContestantEnvelope1Picture).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)ContestantEnvelope4Picture).BeginInit();
             SuspendLayout();
+            // 
+            // HostEnvelope4Picture
+            // 
+            HostEnvelope4Picture.BackColor = Color.White;
+            HostEnvelope4Picture.BackgroundImageLayout = ImageLayout.Center;
+            HostEnvelope4Picture.Location = new Point(1644, 116);
+            HostEnvelope4Picture.Name = "HostEnvelope4Picture";
+            HostEnvelope4Picture.Size = new Size(268, 87);
+            HostEnvelope4Picture.SizeMode = PictureBoxSizeMode.CenterImage;
+            HostEnvelope4Picture.TabIndex = 55;
+            HostEnvelope4Picture.TabStop = false;
+            HostEnvelope4Picture.Tag = "H4";
+            HostEnvelope4Picture.Paint += EnvelopePicture_Paint;
+            // 
+            // HostEnvelope3Picture
+            // 
+            HostEnvelope3Picture.BackColor = Color.White;
+            HostEnvelope3Picture.BackgroundImageLayout = ImageLayout.Center;
+            HostEnvelope3Picture.Location = new Point(1644, 224);
+            HostEnvelope3Picture.Name = "HostEnvelope3Picture";
+            HostEnvelope3Picture.Size = new Size(268, 87);
+            HostEnvelope3Picture.SizeMode = PictureBoxSizeMode.CenterImage;
+            HostEnvelope3Picture.TabIndex = 54;
+            HostEnvelope3Picture.TabStop = false;
+            HostEnvelope3Picture.Tag = "H3";
+            HostEnvelope3Picture.Paint += EnvelopePicture_Paint;
+            // 
+            // HostEnvelope2Picture
+            // 
+            HostEnvelope2Picture.BackColor = Color.White;
+            HostEnvelope2Picture.BackgroundImageLayout = ImageLayout.Center;
+            HostEnvelope2Picture.Location = new Point(1644, 331);
+            HostEnvelope2Picture.Name = "HostEnvelope2Picture";
+            HostEnvelope2Picture.Size = new Size(268, 87);
+            HostEnvelope2Picture.SizeMode = PictureBoxSizeMode.CenterImage;
+            HostEnvelope2Picture.TabIndex = 53;
+            HostEnvelope2Picture.TabStop = false;
+            HostEnvelope2Picture.Tag = "H2";
+            HostEnvelope2Picture.Paint += EnvelopePicture_Paint;
+            // 
+            // HostEnvelope1Picture
+            // 
+            HostEnvelope1Picture.BackColor = Color.White;
+            HostEnvelope1Picture.BackgroundImageLayout = ImageLayout.Center;
+            HostEnvelope1Picture.Location = new Point(1644, 438);
+            HostEnvelope1Picture.Name = "HostEnvelope1Picture";
+            HostEnvelope1Picture.Size = new Size(268, 87);
+            HostEnvelope1Picture.SizeMode = PictureBoxSizeMode.CenterImage;
+            HostEnvelope1Picture.TabIndex = 52;
+            HostEnvelope1Picture.TabStop = false;
+            HostEnvelope1Picture.Tag = "H1";
+            HostEnvelope1Picture.Paint += EnvelopePicture_Paint;
+            // 
+            // HostEnvelope0Picture
+            // 
+            HostEnvelope0Picture.BackColor = Color.White;
+            HostEnvelope0Picture.BackgroundImageLayout = ImageLayout.Center;
+            HostEnvelope0Picture.Location = new Point(1644, 545);
+            HostEnvelope0Picture.Name = "HostEnvelope0Picture";
+            HostEnvelope0Picture.Size = new Size(268, 87);
+            HostEnvelope0Picture.SizeMode = PictureBoxSizeMode.CenterImage;
+            HostEnvelope0Picture.TabIndex = 51;
+            HostEnvelope0Picture.TabStop = false;
+            HostEnvelope0Picture.Tag = "H0";
+            HostEnvelope0Picture.Paint += EnvelopePicture_Paint;
+            // 
+            // ContestantEnvelope0Picture
+            // 
+            ContestantEnvelope0Picture.BackColor = Color.White;
+            ContestantEnvelope0Picture.BackgroundImageLayout = ImageLayout.Center;
+            ContestantEnvelope0Picture.Location = new Point(12, 545);
+            ContestantEnvelope0Picture.Name = "ContestantEnvelope0Picture";
+            ContestantEnvelope0Picture.Size = new Size(268, 87);
+            ContestantEnvelope0Picture.SizeMode = PictureBoxSizeMode.CenterImage;
+            ContestantEnvelope0Picture.TabIndex = 50;
+            ContestantEnvelope0Picture.TabStop = false;
+            ContestantEnvelope0Picture.Tag = "C0";
+            ContestantEnvelope0Picture.Paint += EnvelopePicture_Paint;
+            // 
+            // ContestantEnvelope3Picture
+            // 
+            ContestantEnvelope3Picture.BackColor = Color.White;
+            ContestantEnvelope3Picture.BackgroundImageLayout = ImageLayout.Center;
+            ContestantEnvelope3Picture.Location = new Point(12, 224);
+            ContestantEnvelope3Picture.Name = "ContestantEnvelope3Picture";
+            ContestantEnvelope3Picture.Size = new Size(268, 87);
+            ContestantEnvelope3Picture.SizeMode = PictureBoxSizeMode.CenterImage;
+            ContestantEnvelope3Picture.TabIndex = 49;
+            ContestantEnvelope3Picture.TabStop = false;
+            ContestantEnvelope3Picture.Tag = "C3";
+            ContestantEnvelope3Picture.Paint += EnvelopePicture_Paint;
+            // 
+            // ContestantEnvelope2Picture
+            // 
+            ContestantEnvelope2Picture.BackColor = Color.White;
+            ContestantEnvelope2Picture.BackgroundImageLayout = ImageLayout.Center;
+            ContestantEnvelope2Picture.Location = new Point(12, 331);
+            ContestantEnvelope2Picture.Name = "ContestantEnvelope2Picture";
+            ContestantEnvelope2Picture.Size = new Size(268, 87);
+            ContestantEnvelope2Picture.SizeMode = PictureBoxSizeMode.CenterImage;
+            ContestantEnvelope2Picture.TabIndex = 48;
+            ContestantEnvelope2Picture.TabStop = false;
+            ContestantEnvelope2Picture.Tag = "C2";
+            ContestantEnvelope2Picture.Paint += EnvelopePicture_Paint;
+            // 
+            // ContestantEnvelope1Picture
+            // 
+            ContestantEnvelope1Picture.BackColor = Color.White;
+            ContestantEnvelope1Picture.BackgroundImageLayout = ImageLayout.Center;
+            ContestantEnvelope1Picture.Location = new Point(12, 438);
+            ContestantEnvelope1Picture.Name = "ContestantEnvelope1Picture";
+            ContestantEnvelope1Picture.Size = new Size(268, 87);
+            ContestantEnvelope1Picture.SizeMode = PictureBoxSizeMode.CenterImage;
+            ContestantEnvelope1Picture.TabIndex = 47;
+            ContestantEnvelope1Picture.TabStop = false;
+            ContestantEnvelope1Picture.Tag = "C1";
+            ContestantEnvelope1Picture.Paint += EnvelopePicture_Paint;
+            // 
+            // ContestantEnvelope4Picture
+            // 
+            ContestantEnvelope4Picture.BackColor = Color.White;
+            ContestantEnvelope4Picture.BackgroundImageLayout = ImageLayout.Center;
+            ContestantEnvelope4Picture.Location = new Point(12, 117);
+            ContestantEnvelope4Picture.Name = "ContestantEnvelope4Picture";
+            ContestantEnvelope4Picture.Size = new Size(268, 87);
+            ContestantEnvelope4Picture.SizeMode = PictureBoxSizeMode.CenterImage;
+            ContestantEnvelope4Picture.TabIndex = 46;
+            ContestantEnvelope4Picture.TabStop = false;
+            ContestantEnvelope4Picture.Tag = "C4";
+            ContestantEnvelope4Picture.Paint += EnvelopePicture_Paint;
+            // 
+            // OfferLabel
+            // 
+            OfferLabel.BorderStyle = BorderStyle.FixedSingle;
+            OfferLabel.Font = new Font("Arial", 24F);
+            OfferLabel.ForeColor = Color.White;
+            OfferLabel.Location = new Point(987, 164);
+            OfferLabel.Name = "OfferLabel";
+            OfferLabel.Size = new Size(524, 104);
+            OfferLabel.TabIndex = 45;
+            OfferLabel.Text = "0";
+            OfferLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // CashLabel
+            // 
+            CashLabel.BorderStyle = BorderStyle.FixedSingle;
+            CashLabel.Font = new Font("Arial", 24F);
+            CashLabel.ForeColor = Color.White;
+            CashLabel.Location = new Point(413, 164);
+            CashLabel.Name = "CashLabel";
+            CashLabel.Size = new Size(524, 104);
+            CashLabel.TabIndex = 44;
+            CashLabel.Text = "0";
+            CashLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // OfferTextLabel
+            // 
+            OfferTextLabel.BorderStyle = BorderStyle.FixedSingle;
+            OfferTextLabel.Font = new Font("Arial", 20.25F);
+            OfferTextLabel.ForeColor = Color.White;
+            OfferTextLabel.Location = new Point(987, 116);
+            OfferTextLabel.Name = "OfferTextLabel";
+            OfferTextLabel.Size = new Size(524, 48);
+            OfferTextLabel.TabIndex = 43;
+            OfferTextLabel.Text = "OFERTA";
+            OfferTextLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // CashTextLabel
+            // 
+            CashTextLabel.BorderStyle = BorderStyle.FixedSingle;
+            CashTextLabel.Font = new Font("Arial", 20.25F);
+            CashTextLabel.ForeColor = Color.White;
+            CashTextLabel.Location = new Point(413, 117);
+            CashTextLabel.Name = "CashTextLabel";
+            CashTextLabel.Size = new Size(524, 48);
+            CashTextLabel.TabIndex = 42;
+            CashTextLabel.Text = "GOTÓWKA";
+            CashTextLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // HostTextLabel
+            // 
+            HostTextLabel.Font = new Font("Arial", 20.25F);
+            HostTextLabel.ForeColor = Color.White;
+            HostTextLabel.Location = new Point(1644, 10);
+            HostTextLabel.Name = "HostTextLabel";
+            HostTextLabel.Size = new Size(268, 87);
+            HostTextLabel.TabIndex = 41;
+            HostTextLabel.Text = "PROWADZĄCY";
+            HostTextLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // ContestantTextLabel
+            // 
+            ContestantTextLabel.Font = new Font("Arial", 20.25F);
+            ContestantTextLabel.ForeColor = Color.White;
+            ContestantTextLabel.Location = new Point(12, 10);
+            ContestantTextLabel.Name = "ContestantTextLabel";
+            ContestantTextLabel.Size = new Size(268, 87);
+            ContestantTextLabel.TabIndex = 40;
+            ContestantTextLabel.Text = "ZAWODNIK";
+            ContestantTextLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // AnswerDLabel
+            // 
+            AnswerDLabel.BorderStyle = BorderStyle.FixedSingle;
+            AnswerDLabel.Font = new Font("Arial", 24F);
+            AnswerDLabel.ForeColor = Color.White;
+            AnswerDLabel.Location = new Point(1012, 905);
+            AnswerDLabel.Name = "AnswerDLabel";
+            AnswerDLabel.Size = new Size(900, 95);
+            AnswerDLabel.TabIndex = 37;
+            AnswerDLabel.Text = "D: ?";
+            AnswerDLabel.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // AnswerCLabel
+            // 
+            AnswerCLabel.BorderStyle = BorderStyle.FixedSingle;
+            AnswerCLabel.Font = new Font("Arial", 24F);
+            AnswerCLabel.ForeColor = Color.White;
+            AnswerCLabel.Location = new Point(12, 905);
+            AnswerCLabel.Name = "AnswerCLabel";
+            AnswerCLabel.Size = new Size(900, 95);
+            AnswerCLabel.TabIndex = 36;
+            AnswerCLabel.Text = "C: ?";
+            AnswerCLabel.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // AnswerBLabel
+            // 
+            AnswerBLabel.BorderStyle = BorderStyle.FixedSingle;
+            AnswerBLabel.Font = new Font("Arial", 24F);
+            AnswerBLabel.ForeColor = Color.White;
+            AnswerBLabel.Location = new Point(1012, 802);
+            AnswerBLabel.Name = "AnswerBLabel";
+            AnswerBLabel.Size = new Size(900, 95);
+            AnswerBLabel.TabIndex = 35;
+            AnswerBLabel.Text = "B: ?";
+            AnswerBLabel.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // AnswerALabel
+            // 
+            AnswerALabel.BorderStyle = BorderStyle.FixedSingle;
+            AnswerALabel.Font = new Font("Arial", 24F);
+            AnswerALabel.ForeColor = Color.White;
+            AnswerALabel.Location = new Point(12, 802);
+            AnswerALabel.Name = "AnswerALabel";
+            AnswerALabel.Size = new Size(900, 95);
+            AnswerALabel.TabIndex = 34;
+            AnswerALabel.Text = "A: ?";
+            AnswerALabel.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // QuestionLabel
+            // 
+            QuestionLabel.BorderStyle = BorderStyle.FixedSingle;
+            QuestionLabel.Font = new Font("Arial", 24F);
+            QuestionLabel.ForeColor = Color.White;
+            QuestionLabel.Location = new Point(12, 657);
+            QuestionLabel.Name = "QuestionLabel";
+            QuestionLabel.Size = new Size(1900, 131);
+            QuestionLabel.TabIndex = 33;
+            QuestionLabel.Text = "Pytanie testowe";
+            QuestionLabel.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // ContestantScreenForm
             // 
@@ -37,6 +328,27 @@
             BackColor = Color.Black;
             BackgroundImageLayout = ImageLayout.Center;
             ClientSize = new Size(1924, 1011);
+            Controls.Add(HostEnvelope4Picture);
+            Controls.Add(HostEnvelope3Picture);
+            Controls.Add(HostEnvelope2Picture);
+            Controls.Add(HostEnvelope1Picture);
+            Controls.Add(HostEnvelope0Picture);
+            Controls.Add(ContestantEnvelope0Picture);
+            Controls.Add(ContestantEnvelope3Picture);
+            Controls.Add(ContestantEnvelope2Picture);
+            Controls.Add(ContestantEnvelope1Picture);
+            Controls.Add(ContestantEnvelope4Picture);
+            Controls.Add(OfferLabel);
+            Controls.Add(CashLabel);
+            Controls.Add(OfferTextLabel);
+            Controls.Add(CashTextLabel);
+            Controls.Add(HostTextLabel);
+            Controls.Add(ContestantTextLabel);
+            Controls.Add(AnswerDLabel);
+            Controls.Add(AnswerCLabel);
+            Controls.Add(AnswerBLabel);
+            Controls.Add(AnswerALabel);
+            Controls.Add(QuestionLabel);
             DoubleBuffered = true;
             MaximizeBox = false;
             Name = "ContestantScreenForm";
@@ -45,9 +357,41 @@
             WindowState = FormWindowState.Maximized;
             FormClosing += ContestantScreenForm_FormClosing;
             Load += ContestantScreenForm_Load;
+            ((System.ComponentModel.ISupportInitialize)HostEnvelope4Picture).EndInit();
+            ((System.ComponentModel.ISupportInitialize)HostEnvelope3Picture).EndInit();
+            ((System.ComponentModel.ISupportInitialize)HostEnvelope2Picture).EndInit();
+            ((System.ComponentModel.ISupportInitialize)HostEnvelope1Picture).EndInit();
+            ((System.ComponentModel.ISupportInitialize)HostEnvelope0Picture).EndInit();
+            ((System.ComponentModel.ISupportInitialize)ContestantEnvelope0Picture).EndInit();
+            ((System.ComponentModel.ISupportInitialize)ContestantEnvelope3Picture).EndInit();
+            ((System.ComponentModel.ISupportInitialize)ContestantEnvelope2Picture).EndInit();
+            ((System.ComponentModel.ISupportInitialize)ContestantEnvelope1Picture).EndInit();
+            ((System.ComponentModel.ISupportInitialize)ContestantEnvelope4Picture).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox HostEnvelope4Picture;
+        private PictureBox HostEnvelope3Picture;
+        private PictureBox HostEnvelope2Picture;
+        private PictureBox HostEnvelope1Picture;
+        private PictureBox HostEnvelope0Picture;
+        private PictureBox ContestantEnvelope0Picture;
+        private PictureBox ContestantEnvelope3Picture;
+        private PictureBox ContestantEnvelope2Picture;
+        private PictureBox ContestantEnvelope1Picture;
+        private PictureBox ContestantEnvelope4Picture;
+        private Label OfferLabel;
+        private Label CashLabel;
+        private Label OfferTextLabel;
+        private Label CashTextLabel;
+        private Label HostTextLabel;
+        private Label ContestantTextLabel;
+        private Label AnswerDLabel;
+        private Label AnswerCLabel;
+        private Label AnswerBLabel;
+        private Label AnswerALabel;
+        private Label QuestionLabel;
     }
 }
