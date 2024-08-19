e5b501ee67e14037f3a2af865952e2efdf795749
CMike98 <miccze18@gmail.com>
2024-08-19 13:25:36 +0200

    Settings

--- GameSettings.cs
+++ GameSettings.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CluelessControl
+﻿namespace CluelessControl
 {
     public class GameSettings
     {
+        public int DecimalPlaces
+        {
+            get;
+        }
+
+        public bool FireworksActive
+        {
+            get;
+        }
+
+        public decimal MinimumCashPrizeForFireworks
+        {
+            get;
+        }
+
+        private GameSettings(int decimalPlaces, bool fireworksActive, decimal minimumCashPrizeForFireworks)
+        {
+            DecimalPlaces = decimalPlaces;
+            FireworksActive = fireworksActive;
+            MinimumCashPrizeForFireworks = minimumCashPrizeForFireworks;
+        }
+
+        internal static GameSettings Create()
+        {
+            return new GameSettings(decimalPlaces: 0, fireworksActive: false, minimumCashPrizeForFireworks: 0);
+        }
+
+        internal static GameSettings Create(int decimalPlaces, bool fireworksActive, decimal minimumCashPrizeForFireworks)
+        {
+            if (decimalPlaces < 0)
+                throw new ArgumentOutOfRangeException(nameof(decimalPlaces), $"Number of decimal places in the prize money must be non-negative.");
+            if (minimumCashPrizeForFireworks < 0)
+                throw new ArgumentOutOfRangeException(nameof(minimumCashPrizeForFireworks), $"Minimum cash prize for fireworks must be non-negative.");
+
+            return new GameSettings(decimalPlaces, fireworksActive, minimumCashPrizeForFireworks);
+        }
     }
 }
---
+++ JsonGameSettingsConverter.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CluelessControl
+{
+    public class JsonGameSettingsConverter : JsonConverter<GameSettings>
+    {
+        public override GameSettings? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            using var jsonDoc = JsonDocument.ParseValue(ref reader);
+            var root = jsonDoc.RootElement;
+
+            int decimalPlaces = root.GetProperty("decimalPlaces").GetInt32();
+            bool fireworksActive = root.GetProperty("fireworksActive").GetBoolean();
+            decimal minimumCashPrizeForFireworks = root.GetProperty("minimumCashPrizeForFireworks").GetDecimal();
+
+            return GameSettings.Create(decimalPlaces, fireworksActive, minimumCashPrizeForFireworks);
+        }
+
+        public override void Write(Utf8JsonWriter writer, GameSettings value, JsonSerializerOptions options)
+        {
+            writer.WriteStartObject();
+
+            writer.WriteNumber("decimalPlaces", value.DecimalPlaces);
+            writer.WriteBoolean("fireworksActive", value.FireworksActive);
+            writer.WriteNumber("minimumCashPrizeForFireworks", value.MinimumCashPrizeForFireworks);
+
+            writer.WriteEndObject();
+        }
+    }
+}
