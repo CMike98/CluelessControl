9aa91368fe80f3cc8468adb3fa8bd2128bb6293f
CMike98 <miccze18@gmail.com>
2024-08-19 19:26:04 +0200

    Add the new ChequeSettings class

---
+++ ChequeSettings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CluelessControl
+{
+    public class ChequeSettings
+    {
+        public List<BaseCheque> ChequeList
+        {
+            get;
+            private set;
+        }
+
+        private ChequeSettings(List<BaseCheque> chequeList)
+        {
+            ChequeList = chequeList;
+        }
+
+        internal static ChequeSettings Create()
+        {
+            return new ChequeSettings(new List<BaseCheque>());
+        }
+
+        internal static ChequeSettings Create(List<BaseCheque> chequeList)
+        {
+            if (chequeList == null)
+                throw new ArgumentNullException(nameof(chequeList));
+            if (chequeList.Any(cheque => cheque == null))
+                throw new ArgumentException($"At least one of the cheques on the list is null.", nameof(chequeList));
+            return new ChequeSettings(chequeList);
+        }
+    }
+}
---
+++ JsonChequeSettingsConverter.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CluelessControl
+{
+    public class JsonChequeSettingsConverter : JsonConverter<ChequeSettings>
+    {
+        public override ChequeSettings? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            using var jsonDoc = JsonDocument.ParseValue(ref reader);
+            var root = jsonDoc.RootElement;
+
+            List<BaseCheque> cheques = root.GetProperty("chequeList")
+                .EnumerateArray()
+                .Select(chequeElement =>
+                {
+                    return JsonSerializer.Deserialize<BaseCheque>(chequeElement, options) ?? throw new JsonException($"Invalid cheque list JSON.");
+                })
+                .ToList();
+
+            return ChequeSettings.Create(cheques);
+        }
+
+        public override void Write(Utf8JsonWriter writer, ChequeSettings value, JsonSerializerOptions options)
+        {
+            // Start writing the cheque list
+            writer.WriteStartObject();
+
+            // Write the chequeList property
+            writer.WritePropertyName("chequeList");
+
+            // Start writing the cheque array
+            writer.WriteStartArray();
+
+            // Write each cheque inside
+            foreach (BaseCheque cheque in value.ChequeList)
+            {
+                JsonSerializer.Serialize(writer, cheque, options);
+            }
+
+            // End writing the array
+            writer.WriteEndArray();
+
+            // End writing the whole list
+            writer.WriteEndObject();
+        }
+    }
+}
