82ee7219dce061bf8298ab7d159bf5cd528af25e
CMike98 <miccze18@gmail.com>
2024-08-26 20:10:58 +0200

    Utils class created

---
+++ Utils.cs
@@ -0,0 +1,19 @@
+﻿namespace CluelessControl
+{
+    public static class Utils
+    {
+        public static string AmountToString(decimal amount)
+        {
+            if (amount % 1 == 0)
+                return amount.ToString("#,##0");
+            else
+                return amount.ToString("#,##0.00######");
+        }
+
+        public static string PercentageToString(decimal percentage)
+        {
+            decimal fraction = percentage / 100;
+            return fraction.ToString("+#,##0.##########%;-#,##0.##########%");
+        }
+    }
+}
