f0c807c3288db340444c9f8f521b0363156c2ce8
CMike98 <miccze18@gmail.com>
2024-08-28 16:32:11 +0200

    Add regions and delete usings

--- Constants.cs
+++ Constants.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Policy;
-using System.Text;
-using System.Text.Encodings.Web;
+﻿using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Text.Unicode;
-using System.Threading.Tasks;
 
 namespace CluelessControl
 {
     public static class Constants
     {
+        #region Strings
         /// <summary>
         /// The title of the program.
         /// </summary>
@@ -23,6 +18,9 @@ namespace CluelessControl
         /// </summary>
         public const string CLOSE_ON_DIRECTOR_FORM_MESSAGE = "Zamknij reżyserkę, by zamknąć program.";
 
+        #endregion
+
+        #region Envelopes and cheques
         /// <summary>
         /// How much envelopes does the contestant select by default?
         /// </summary>
@@ -52,6 +50,8 @@ namespace CluelessControl
         /// Minimum percentage cheque
         /// </summary>
         public const decimal MIN_PERCENTAGE = -100;
+        #endregion
+
 
         #region Drawing
         /// <summary>
