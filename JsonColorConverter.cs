56fa0570cca28619370968c86e348e2d3f5ef7ba
CMike98 <miccze18@gmail.com>
2024-09-05 11:35:17 +0200

    Add color converter

---
+++ JsonColorConverter.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CluelessControl
+{
+    public class JsonColorConverter : JsonConverter<Color>
+    {
+        public override Color Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            using var jsonDoc = JsonDocument.ParseValue(ref reader);
+            var root = jsonDoc.RootElement;
+
+            byte a = root.GetProperty("a").GetByte();
+            byte r = root.GetProperty("r").GetByte();
+            byte g = root.GetProperty("g").GetByte();
+            byte b = root.GetProperty("b").GetByte();
+
+            return Color.FromArgb(a, r, g, b);
+        }
+
+        public override void Write(Utf8JsonWriter writer, Color value, JsonSerializerOptions options)
+        {
+            writer.WriteStartObject();
+
+            writer.WriteNumber("a", value.A);
+            writer.WriteNumber("r", value.R);
+            writer.WriteNumber("g", value.G);
+            writer.WriteNumber("b", value.B);
+
+            writer.WriteEndObject();
+        }
+    }
+}
