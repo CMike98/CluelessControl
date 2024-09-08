4bff5f5b1621874160eea457deddacaa3bd33271
CMike98 <miccze18@gmail.com>
2024-09-08 18:52:27 +0200

    Eliminate unneeded usings

--- Envelopes/EnvelopeCalculator.cs
+++ Envelopes/EnvelopeCalculator.cs
@@ -1,9 +1,4 @@
 ﻿using CluelessControl.Cheques;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CluelessControl.Envelopes
 {
