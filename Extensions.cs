672d55b12bf1616d2572b18bc244c415e516c73a
CMike98 <miccze18@gmail.com>
2024-09-16 17:05:50 +0200

    Add extensions calculating a product

---
+++ Extensions.cs
@@ -0,0 +1,37 @@
+﻿namespace CluelessControl
+{
+    public static class Extensions
+    {
+        public static decimal Product(this IEnumerable<decimal> numbers)
+        {
+            if (numbers is null)
+                throw new ArgumentNullException(nameof(numbers));
+
+            decimal result = 1;
+
+            foreach (decimal number in numbers)
+            {
+                result *= number;
+            }
+
+            return result;
+        }
+
+        public static decimal Product<T>(this IEnumerable<T> items, Func<T, decimal> converter)
+        {
+            if (items is null)
+                throw new ArgumentNullException(nameof(items));
+            if (converter is null)
+                throw new ArgumentNullException(nameof(converter));
+
+            decimal result = 1;
+
+            foreach (T item in items)
+            {
+                result *= converter(item);
+            }
+
+            return result;
+        }
+    }
+}
