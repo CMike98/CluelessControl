4ff5941ed2e15b924e308c5141a8a2ac9e22bed7
CMike98 <miccze18@gmail.com>
2024-10-04 14:42:08 +0200

    Change to TextRenderer and make limiting the scale optional

--- FontHelper.cs
+++ FontHelper.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CluelessControl
+﻿namespace CluelessControl
 {
     public static class FontHelper
     {
-        public static Font GetMaxFont(string s, Graphics graphics, Font font, SizeF maxSize)
+        public static Font GetMaxFont(string s, Graphics graphics, Font font, Size maxSize, TextFormatFlags flags, bool limitScale = true)
         {
             if (s is null)
                 throw new ArgumentNullException(nameof(s));
@@ -19,13 +13,14 @@ namespace CluelessControl
             if (maxSize.IsEmpty)
                 throw new ArgumentException($"The max size variable is empty!", nameof(maxSize));
 
-            SizeF textSize = graphics.MeasureString(s, font);
+            SizeF textSize = TextRenderer.MeasureText(s, font, maxSize, flags);
 
             float scale = Math.Min(maxSize.Height / textSize.Height, maxSize.Width / textSize.Width);
-            if (scale > 1)
+
+            if (limitScale && scale > 1)
                 scale = 1;
 
-            int fontSize = (int) Math.Round(font.Size * scale);
+            int fontSize = (int) (font.Size * scale);
             return new Font(font.FontFamily, fontSize, font.Style);
         }
     }
