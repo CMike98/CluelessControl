ce62d8fa35d77e9a81f86dc5e56fe6f214a6f9b4
CMike98 <miccze18@gmail.com>
2025-03-02 19:25:10 +0100

    Add a new form that allows to add a new envelope

---
+++ AddEnvelopeForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace CluelessControl
+{
+    partial class AddEnvelopeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            AddEnvelopeChooseEnvelopeLbl = new Label();
+            AddEnvelopeSelectListBox = new ListBox();
+            AddEnvelopeSelectedEnvelopeLbl = new Label();
+            AddEnvelopeSelectedTitleLbl = new Label();
+            AddEnvelopeConfirmBtn = new Button();
+            SuspendLayout();
+            // 
+            // AddEnvelopeChooseEnvelopeLbl
+            // 
+            AddEnvelopeChooseEnvelopeLbl.Font = new Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            AddEnvelopeChooseEnvelopeLbl.Location = new Point(12, 9);
+            AddEnvelopeChooseEnvelopeLbl.Name = "AddEnvelopeChooseEnvelopeLbl";
+            AddEnvelopeChooseEnvelopeLbl.Size = new Size(204, 43);
+            AddEnvelopeChooseEnvelopeLbl.TabIndex = 0;
+            AddEnvelopeChooseEnvelopeLbl.Text = "Wybierz kopertę:";
+            AddEnvelopeChooseEnvelopeLbl.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // AddEnvelopeSelectListBox
+            // 
+            AddEnvelopeSelectListBox.Font = new Font("Arial", 14.25F);
+            AddEnvelopeSelectListBox.FormattingEnabled = true;
+            AddEnvelopeSelectListBox.ItemHeight = 22;
+            AddEnvelopeSelectListBox.Location = new Point(12, 55);
+            AddEnvelopeSelectListBox.Name = "AddEnvelopeSelectListBox";
+            AddEnvelopeSelectListBox.Size = new Size(204, 158);
+            AddEnvelopeSelectListBox.TabIndex = 56;
+            AddEnvelopeSelectListBox.SelectedIndexChanged += AddEnvelopeSelectListBox_SelectedIndexChanged;
+            // 
+            // AddEnvelopeSelectedEnvelopeLbl
+            // 
+            AddEnvelopeSelectedEnvelopeLbl.BorderStyle = BorderStyle.FixedSingle;
+            AddEnvelopeSelectedEnvelopeLbl.Font = new Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            AddEnvelopeSelectedEnvelopeLbl.Location = new Point(222, 55);
+            AddEnvelopeSelectedEnvelopeLbl.Name = "AddEnvelopeSelectedEnvelopeLbl";
+            AddEnvelopeSelectedEnvelopeLbl.Size = new Size(268, 87);
+            AddEnvelopeSelectedEnvelopeLbl.TabIndex = 58;
+            AddEnvelopeSelectedEnvelopeLbl.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // AddEnvelopeSelectedTitleLbl
+            // 
+            AddEnvelopeSelectedTitleLbl.Font = new Font("Arial", 12F);
+            AddEnvelopeSelectedTitleLbl.Location = new Point(222, 9);
+            AddEnvelopeSelectedTitleLbl.Name = "AddEnvelopeSelectedTitleLbl";
+            AddEnvelopeSelectedTitleLbl.Size = new Size(268, 46);
+            AddEnvelopeSelectedTitleLbl.TabIndex = 57;
+            AddEnvelopeSelectedTitleLbl.Text = "Wybrana koperta:";
+            AddEnvelopeSelectedTitleLbl.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // AddEnvelopeConfirmBtn
+            // 
+            AddEnvelopeConfirmBtn.Enabled = false;
+            AddEnvelopeConfirmBtn.Font = new Font("Arial", 12F);
+            AddEnvelopeConfirmBtn.Location = new Point(292, 145);
+            AddEnvelopeConfirmBtn.Name = "AddEnvelopeConfirmBtn";
+            AddEnvelopeConfirmBtn.Size = new Size(128, 68);
+            AddEnvelopeConfirmBtn.TabIndex = 59;
+            AddEnvelopeConfirmBtn.Text = "Zatwierdź";
+            AddEnvelopeConfirmBtn.UseVisualStyleBackColor = true;
+            AddEnvelopeConfirmBtn.Click += AddEnvelopeConfirmBtn_Click;
+            // 
+            // AddEnvelopeForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(509, 225);
+            Controls.Add(AddEnvelopeConfirmBtn);
+            Controls.Add(AddEnvelopeSelectedEnvelopeLbl);
+            Controls.Add(AddEnvelopeSelectedTitleLbl);
+            Controls.Add(AddEnvelopeSelectListBox);
+            Controls.Add(AddEnvelopeChooseEnvelopeLbl);
+            DoubleBuffered = true;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "AddEnvelopeForm";
+            Text = "Dodaj kopertę";
+            TopMost = true;
+            FormClosing += AddEnvelopeForm_FormClosing;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label AddEnvelopeChooseEnvelopeLbl;
+        private ListBox AddEnvelopeSelectListBox;
+        private Label AddEnvelopeSelectedEnvelopeLbl;
+        private Label AddEnvelopeSelectedTitleLbl;
+        private Button AddEnvelopeConfirmBtn;
+    }
+}
---
+++ AddEnvelopeForm.cs
@@ -0,0 +1,92 @@
+﻿using CluelessControl.Envelopes;
+
+namespace CluelessControl
+{
+    public partial class AddEnvelopeForm : Form
+    {
+        private TradingSide _side;
+        private bool _isShowing;
+        private List<int> _envelopeNumbers;
+
+        public bool IsShowing => _isShowing;
+
+        public AddEnvelopeForm()
+        {
+            InitializeComponent();
+            _envelopeNumbers = new List<int>(capacity: Constants.GameConstants.MAX_ENVELOPES_COUNT);
+        }
+
+        public void Reset(TradingSide side)
+        {
+            // Clear the AddEnvelopeSelectListBox item
+            AddEnvelopeSelectListBox.SelectedIndex = -1;
+
+            // Clear the envelope label
+            AddEnvelopeSelectedEnvelopeLbl.Text = string.Empty;
+
+            // Add the envelopes to AddEnvelopeSelectListBox
+            _envelopeNumbers.Clear();
+
+            AddEnvelopeSelectListBox.Items.Clear();
+            foreach (Envelope envelope in GameState.Instance.EnvelopeTable.EnvelopesOnTable)
+            {
+                AddEnvelopeSelectListBox.Items.Add($"{envelope.EnvelopeNumber}. {envelope.Cheque.ToValueString()}");
+                _envelopeNumbers.Add(envelope.EnvelopeNumber);
+            }
+
+            // And save the current side for future reference
+            _side = side;
+
+            _isShowing = true;
+        }
+
+        private void AddEnvelopeSelectListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = AddEnvelopeSelectListBox.SelectedIndex;
+            if (index == -1)
+            {
+                AddEnvelopeSelectedEnvelopeLbl.Text = string.Empty;
+                AddEnvelopeConfirmBtn.Enabled = false;
+                return;
+            }
+
+            int envelopeNumber = _envelopeNumbers[index];
+            Envelope envelope = GameState.Instance.EnvelopeTable.GetEnvelope(envelopeNumber);
+            AddEnvelopeSelectedEnvelopeLbl.Text = envelope.GetEnvelopeValueForDirector();
+            AddEnvelopeConfirmBtn.Enabled = true;
+        }
+
+        private void AddEnvelopeConfirmBtn_Click(object sender, EventArgs e)
+        {
+            var gameStateInstance = GameState.Instance;
+
+            int index = AddEnvelopeSelectListBox.SelectedIndex;
+            int envelopeNumber = _envelopeNumbers[index];
+            Envelope newEnvelope = gameStateInstance.EnvelopeTable.GetEnvelope(envelopeNumber);
+            newEnvelope.MarkAsNeutral();
+
+            EnvelopeSet set = _side == TradingSide.Contestant ? gameStateInstance.ContestantEnvelopeSet : gameStateInstance.HostEnvelopeSet;
+            set.AddEnvelope(newEnvelope);
+
+            Program.DirectorForm.TradingUpdateEnvelopes();
+            Program.DirectorForm.TradingUpdateCashLabels();
+            Program.DirectorForm.TradingClearCheckboxes();
+            Program.DirectorForm.TradingUpdatePages();
+
+            gameStateInstance.EnvelopeTable.DeleteEnvelope(newEnvelope);
+            gameStateInstance.RefreshEnvelopes();
+            
+            _isShowing = false;
+
+            this.OnFormClosing(new FormClosingEventArgs(closeReason: CloseReason.UserClosing, cancel: true));
+        }
+
+        private void AddEnvelopeForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _isShowing = false;
+
+            Hide();
+            e.Cancel = true;
+        }
+    }
+}
