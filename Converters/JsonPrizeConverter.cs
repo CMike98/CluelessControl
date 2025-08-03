a04bef9674e90e9a5cba04913ee0c262d774fcde
CMike98 <miccze18@gmail.com>
2025-08-03 20:39:29 +0200

    Json prize converters

---
+++ Converters/JsonPrizeConverter.cs
@@ -0,0 +1,32 @@
+﻿using CluelessControl.Prizes;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CluelessControl.Converters
+{
+    public class JsonPrizeConverter : JsonConverter<Prize>
+    {
+        public override Prize? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            using var jsonDoc = JsonDocument.ParseValue(ref reader);
+            var root = jsonDoc.RootElement;
+
+            string prizeName = root.GetProperty("PrizeName").GetString() ?? throw new JsonException("No prize code.");
+            decimal roundingUnit = root.GetProperty("RoundingUnit").GetDecimal();
+            RoundingMethod roundingMethod = (RoundingMethod) root.GetProperty("RoundingMethod").GetInt32();
+
+            return Prize.CreatePrize(prizeName, roundingUnit, roundingMethod);
+        }
+
+        public override void Write(Utf8JsonWriter writer, Prize value, JsonSerializerOptions options)
+        {
+            writer.WriteStartObject();
+
+            writer.WriteString("PrizeName", value.PrizeName);
+            writer.WriteNumber("RoundingUnit", value.RoundingUnit);
+            writer.WriteNumber("RoundingMethod", (int)value.RoundingMethod);
+
+            writer.WriteEndObject();
+        }
+    }
+}
---
+++ Converters/JsonPrizeListConverter.cs
@@ -0,0 +1,45 @@
+﻿using CluelessControl.Prizes;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CluelessControl.Converters
+{
+    public class JsonPrizeListConverter : JsonConverter<PrizeList>
+    {
+        public override PrizeList? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            using var jsonDoc = JsonDocument.ParseValue(ref reader);
+            var root = jsonDoc.RootElement;
+
+            Dictionary<string, Prize> prizes = root.GetProperty("PrizeList").EnumerateArray().Select(prizeElement =>
+            {
+                string key = prizeElement.GetProperty("PrizeKey").GetString() ?? throw new JsonException($"Prize key missing.");
+                Prize prize = prizeElement.GetProperty("Prize").Deserialize<Prize>(options) ?? throw new JsonException($"Invalid prize JSON.");
+                return (key, prize);
+            }).ToDictionary();
+
+            return PrizeList.Create(prizes);
+        }
+
+        public override void Write(Utf8JsonWriter writer, PrizeList value, JsonSerializerOptions options)
+        {
+            writer.WriteStartObject();
+
+            writer.WritePropertyName("PrizeList");
+            writer.WriteStartArray();
+
+            foreach (KeyValuePair<string, Prize> prize in value.PrizeDictionary)
+            {
+                writer.WriteString("PrizeKey", prize.Key);
+
+                writer.WritePropertyName("Prize");
+                writer.WriteStartObject();
+                JsonSerializer.Serialize(writer, prize.Value, prize.Value.GetType(), options);
+                writer.WriteEndObject();
+            }
+
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+        }
+    }
+}
