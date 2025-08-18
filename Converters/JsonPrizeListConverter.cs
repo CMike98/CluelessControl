26edad321ae6e1ee8f0c3c3f057b66ef8990bd43
CMike98 <miccze18@gmail.com>
2025-08-18 21:57:33 +0200

    Converter properties renamed

--- Converters/JsonPrizeListConverter.cs
+++ Converters/JsonPrizeListConverter.cs
@@ -13,7 +13,7 @@ namespace CluelessControl.Converters
 
             Dictionary<string, PrizeData> prizes = root.GetProperty("PrizeList").EnumerateArray().Select(prizeElement =>
             {
-                string key = prizeElement.GetProperty("PrizeKey").GetString() ?? throw new JsonException($"PrizeData key missing.");
+                string key = prizeElement.GetProperty("PrizeCode").GetString() ?? throw new JsonException($"PrizeData key missing.");
                 PrizeData prize = prizeElement.GetProperty("PrizeData").Deserialize<PrizeData>(options) ?? throw new JsonException($"Invalid prize JSON.");
                 return (key, prize);
             }).ToDictionary();
@@ -30,11 +30,13 @@ namespace CluelessControl.Converters
 
             foreach (KeyValuePair<string, PrizeData> prize in value.PrizeDictionary)
             {
-                writer.WriteString("PrizeKey", prize.Key);
+                writer.WriteStartObject();
+
+                writer.WriteString("PrizeCode", prize.Key);
 
                 writer.WritePropertyName("PrizeData");
-                writer.WriteStartObject();
                 JsonSerializer.Serialize(writer, prize.Value, prize.Value.GetType(), options);
+
                 writer.WriteEndObject();
             }
 
